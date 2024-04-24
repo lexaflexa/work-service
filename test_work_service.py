4cca36a2e6c97b236d2f0f412e7b9378dc4a7aee
lexaflexa <whatcodingrose@gmail.com>
2024-04-24 21:43:42 +0300

    2

--- test_work_service.py
+++ test_work_service.py
@@ -19,10 +19,10 @@ def test_create_client(workers: WorkIn = workers):
 
 def test_update_client_age(workers: WorkIn = workers):
     workers_upd = WorkOut(
-        name='Алексей Петров',
-        phone='+71212121212',
-        pay='50000',
-        genre='Мужчина',
+        name='Юлия Иванова',
+        phone='+7143658769',
+        pay='10000',
+        genre='Женщина',
         id=1
     )
     assert dict(workers_upd) == {'name': workers.name,
@@ -35,10 +35,10 @@ def test_update_client_age(workers: WorkIn = workers):
 
 def test_update_client_genre(workers: WorkIn = workers):
     workers_upd = WorkOut(
-        name='Алексей Петров',
-        phone='+71212121212',
-        pay='50000',
-        genre='Мужчина',
+        name='Юлия Иванова',
+        phone='+7143658769',
+        pay='10000',
+        genre='Женщина',
         id=1
     )
     assert dict(workers_upd) == {'name': workers.name,
